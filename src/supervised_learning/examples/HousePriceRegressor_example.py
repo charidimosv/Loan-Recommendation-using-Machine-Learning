406b22a6448958145db82b59b354e5b061085a9f
Charidimos Vasilikakis <ch.vasilikakis@outlook.com>
2018-05-30 11:41:43 +0300

    attempt for heroku

--- src/supervised_learning/examples/HousePriceRegressor_example.py
+++ src/supervised_learning/examples/HousePriceRegressor_example.py
@@ -7,7 +7,6 @@ from sklearn.linear_model import ElasticNet, Lasso
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import RobustScaler
 
-from src.supervised_learning.models.AveragingModels import AveragingModels
 from src.supervised_learning.models.HousePriceRegressor import HousePriceRegressor
 from src.supervised_learning.models.StackingAveragedModels import StackingAveragedModels
 from src.utils.conf import *
@@ -41,32 +40,33 @@ if __name__ == "__main__":
     # lasso = Lasso(alpha=0.00042, random_state=1, max_iter=50000)
     # house_regr.print_rmsle_cv(lasso)
     house_regr.fit(_model=lasso)
+    test_pred = house_regr.predict(df_test)
 
-    temp = df_test.head(1)
-    allDATA = pd.concat((df_test, temp)).reset_index(drop=True)
-    temp = allDATA[df_test.shape[0]:]
-
-    print(house_regr.predict(_df_test=allDATA)[df_test.shape[0]:])
-    # print(house_regr.predict(_df_test=allDATA))
-    # print(house_regr.predict(_df_test=allDATA))
-
-
-
-
-    elasticNet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0006, l1_ratio=1, random_state=3, max_iter=50000))
+    # temp = df_test.head(1)
+    # allDATA = pd.concat((df_test, temp)).reset_index(drop=True)
+    # temp = allDATA[df_test.shape[0]:]
+    #
+    # print(house_regr.predict(_df_test=allDATA)[df_test.shape[0]:])
+    # # print(house_regr.predict(_df_test=allDATA))
+    # # print(house_regr.predict(_df_test=allDATA))
+    #
+    #
+    #
+    #
+    # elasticNet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0006, l1_ratio=1, random_state=3, max_iter=50000))
     elasticNet = ElasticNet(alpha=0.0006, l1_ratio=1, random_state=3, max_iter=50000)
-    house_regr.print_rmsle_cv(elasticNet)
-
-    averaged_models = AveragingModels(models=(elasticNet, lasso))
-    house_regr.print_rmsle_cv(averaged_models)
-
+    # house_regr.print_rmsle_cv(elasticNet)
+    #
+    # averaged_models = AveragingModels(models=(elasticNet, lasso))
+    # house_regr.print_rmsle_cv(averaged_models)
+    #
     KRR = KernelRidge(alpha=0.6, kernel="polynomial", degree=2, coef0=2.5)
     GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
                                        max_depth=4, max_features="sqrt",
                                        min_samples_leaf=15, min_samples_split=10,
                                        loss="huber", random_state=5)
     stacked_averaged_models = StackingAveragedModels(base_models=(elasticNet, GBoost, KRR), meta_model=lasso)
-    house_regr.print_rmsle_cv(stacked_averaged_models)
+    # house_regr.print_rmsle_cv(stacked_averaged_models)
 
     # house_regr.print_rmsle_cv(stacked_averaged_models)
     # house_regr.print_rmsle_cv(lr)
@@ -78,5 +78,5 @@ if __name__ == "__main__":
 
     # house_regr.prepare_test_data(df_test)
 
-    # test_pred = house_regr.predict()
-    test_pred = house_regr.predict(df_test)
+    house_regr.fit(_model=stacked_averaged_models)
+    test_pred = house_regr.predict()
